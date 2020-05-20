48b0ec40112855b4d3d80af014978f0e09c3a3e5
Jiri Srba <jiri.srba@gmail.com>
2020-05-20 20:42:46 +0200

    ci

---
+++ install.sql
@@ -0,0 +1,12 @@
+\i view/redim_users.sql
+\i view/redim_roles.sql
+\i view/redim_user_roles.sql
+
+\i function/get_version.sql
+\i function/run_sql.sql
+\i function/create_db_user.sql
+\i function/update_user_profile.sql
+\i function/reset_user_password.sql
+\i function/remove_db_user.sql
+\i function/grant_role.sql
+\i function/revoke_role.sql
